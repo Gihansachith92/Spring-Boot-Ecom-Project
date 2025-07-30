cb6ef22e3315830d5d2d5a66075df4405415422b
Gihan_Sachith <gihansachith92@gmail.com>
2025-07-30 20:39:08 +0530

    search product logic in controller

--- src/main/java/com/gihan/ecomproject/controller/ProductController.java
+++ src/main/java/com/gihan/ecomproject/controller/ProductController.java
@@ -79,4 +79,10 @@ public class ProductController {
 
     }
 
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
+        List<Product> products = service.seacrhProducts(keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
 }
